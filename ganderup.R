249e62dc0e7db53feeb3676fb07b89e0e60fc11a
F. Ozge Ozturk <ozozge@gmail.com>
2025-03-18 11:26:28 -0400

    Add files via upload

---
+++ ganderup.R
@@ -0,0 +1,9 @@
+#install.packages('ellmer', dependencies=TRUE, repos='http://cran.rstudio.com/')
+#install.packages('gander', dependencies=TRUE, repos='http://cran.rstudio.com/')
+
+library(gander)
+library(ellmer)
+
+options(.gander_chat = ellmer::chat_claude())
+
+#Sys.setenv(ANTHROPIC_API_KEY = "key")
